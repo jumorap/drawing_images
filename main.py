29cc9089a423c14f9d7bc2ae8a5472f979196bf1
Jumorap <jumorap@unal.edu.co>
2020-10-11 17:28:59 -0500

    Was added documentation, one part of UI, where the user can look the image result without open the file directly. Was looked that while better quality (inclusive size) have the image, we can generate a better result with more details.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PIL import Image
 
 
 def dodge(front, back):
+    # We force the blur to return us the value 255 if is > than 255
     final_sketch = front * 255 / (255 - back)
     final_sketch[final_sketch > 255] = 255
     final_sketch[back == 255] = 255
@@ -15,11 +16,14 @@ def dodge(front, back):
 
 
 def image_selector():
+    # Using exceptions (can be better optimized), we select the file called
+    # 'photo' in png format. Additional to it, we require to our users that
+    # introduce a sigma value [my_sigma], this value is the intensity of blurness
     try:
-        img = 'assets\\photo.png'
+        img = 'assets\\proof.png'
         save_img = Image.open(img)
     except:
-        img = 'assets\\photo.jpg'
+        img = 'assets\\proof.jpg'
         save_img = Image.open(img)
         save_img.save('assets\\photo.png')
     finally:
@@ -34,6 +38,9 @@ def image_selector():
 
 
 def variables_process(image, my_sigma_format):
+    # Read the image and is processed like a image in gray scale
+    # After it we load the sigma value in [my_sigma_format], that
+    # is a value that ingress or user
     rgb = imageio.imread(image)
     rgb_gray = np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
     img_residual = 255 - rgb_gray
@@ -44,8 +51,18 @@ def variables_process(image, my_sigma_format):
 
 
 if __name__ == "__main__":
+    '''While better quality (inclusive size) have the image, is possible build image with more details'''
+    # After call [image_selector()], that return a vector of 2 components
+    # we use 2 indicators to give name to image result, the first is actual
+    # date using 'time' library, and sigma value [image_result[1]]
     image_result = image_selector()
     timer = str(time.strftime("%d%m%y%H%M%S")) + str(image_result[1])
-    str_result = 'results/result%d.png' % int(timer)
+    my_time = int(timer)
+    str_result = 'results/result%d.png' % my_time
+    file = r'results/result%d.png' % my_time
 
+    # Image result is saved and after showed to our users in another window
     cv2.imwrite(str_result, image_result[0])
+    reader = cv2.imread(file)
+    cv2.imshow('Result', reader)
+    cv2.waitKey()
